e9287e541e8e0160e04863024bfdaac88d2b0ab0
EddieO23 <eocampo52@gmail.com>
2023-12-21 18:47:02 -0600

    working on javascript

---
+++ java.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container"),
+selectXBtn = container.querySelector(".playerX")
+selectOBtn = container.querySelector(".playerO");
+
+window.onload = () => {
+    
+}
